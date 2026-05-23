abdcf754f2d1c590147ec0c555f00c87cf1c12ca
RAINER <50297972+rainer2050@users.noreply.github.com>
2026-05-22 19:24:27 -0500

    rv4

--- map.py
+++ map.py
@@ -252,7 +252,7 @@ if st.session_state.get("logueado"):
     f"plus/Reportes/"
     f"ajax_ordenes_historico_xls/"
     f"U/{hoy}/{hoy}/0/0/0/"
-    f"{ruta}/0/0/0/0/0/0/9/{periodos_seleccionados}"
+    f"{ruta}/0/0/0/0/0/0/9/{periodos_seleccionados[-1]}"
 )
             st.code(url_pendientes)
 
