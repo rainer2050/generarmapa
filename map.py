4fc6bbb91f2265c7fc48f8c09ec12ce414dc34af
RAINER <50297972+rainer2050@users.noreply.github.com>
2026-05-25 12:02:44 -0500

    ascac

--- map.py
+++ map.py
@@ -785,7 +785,7 @@ if "df_final" in st.session_state:
             color:black;
             font-weight:bold;
             white-space: nowrap;
-            margin-left:12px;
+            margin-left:8px;
             margin-top:-2px;
         ">
             {row[col_suministro]}
