be9c5fa829b61dc2099aeb7138b4cc484c0fd36f
RAINER <50297972+rainer2050@users.noreply.github.com>
2026-05-25 16:18:24 -0500

    radada

--- map.py
+++ map.py
@@ -404,7 +404,7 @@ if st.session_state["logueado"]:
                     )
                 ).add_to(cluster)
 
-            folium.LayerControl().add_to(mapa)
+            folium.LayerControl(position="topright", collapsed=True).add_to(mapa)
             mapa_html = mapa._repr_html_()
             components.html(mapa_html, height=850, scrolling=True)
 
